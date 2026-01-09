94abbc37479a6830fd74563ff281e3b42ee0e338
MaGi7 <magi@jis.az>
2026-01-09 22:43:06 +0400

    Add files via upload

---
+++ domino-online/public/game.js
@@ -0,0 +1,33 @@
+const socket = io();
+
+function createRoom() {
+  socket.emit("createRoom", {
+    name: name.value,
+    password: createPass.value
+  });
+}
+
+function joinRoom() {
+  socket.emit("joinRoom", {
+    roomCode: roomCode.value.toUpperCase(),
+    name: name.value,
+    password: joinPass.value
+  });
+}
+
+socket.on("roomCreated", code => {
+  alert("Room Code: " + code);
+});
+
+socket.on("playersUpdate", players => {
+  playersList.innerHTML = "";
+  players.forEach(p => {
+    const li = document.createElement("li");
+    li.textContent = `${p.name} (${p.score})`;
+    playersList.appendChild(li);
+  });
+});
+
+socket.on("errorMsg", msg => alert(msg));
+
+const playersList = document.getElementById("players");
---
+++ domino-online/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+const rooms = {};
+
+function generateRoomCode() {
+  return Math.random().toString(36).substring(2, 7).toUpperCase();
+}
+
+io.on("connection", socket => {
+
+  socket.on("createRoom", ({ name, password }) => {
+    const roomCode = generateRoomCode();
+    rooms[roomCode] = {
+      password,
+      players: [{ id: socket.id, name, score: 0 }]
+    };
+    socket.join(roomCode);
+    socket.emit("roomCreated", roomCode);
+    io.to(roomCode).emit("playersUpdate", rooms[roomCode].players);
+  });
+
+  socket.on("joinRoom", ({ roomCode, name, password }) => {
+    const room = rooms[roomCode];
+    if (!room) return socket.emit("errorMsg", "Room not found");
+    if (room.password !== password)
+      return socket.emit("errorMsg", "Wrong password");
+    if (room.players.length >= 5)
+      return socket.emit("errorMsg", "Room full");
+
+    room.players.push({ id: socket.id, name, score: 0 });
+    socket.join(roomCode);
+    io.to(roomCode).emit("playersUpdate", room.players);
+  });
+
+  socket.on("disconnect", () => {
+    for (const code in rooms) {
+      rooms[code].players = rooms[code].players.filter(p => p.id !== socket.id);
+      if (rooms[code].players.length === 0) delete rooms[code];
+      else io.to(code).emit("playersUpdate", rooms[code].players);
+    }
+  });
+
+});
+
+server.listen(process.env.PORT || 3000, () =>
+  console.log("Server running")
+);
